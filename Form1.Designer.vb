e66552c284b3dbd3e04926dea5ca6c5b0a4e42cd
BeratDizdar <beratdizdar2004@gmail.com>
2023-11-27 22:59:23 +0300

    yeni özellikler

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -27,15 +27,19 @@ Partial Class Form1
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.DosyaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.KaydetToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.TextBox = New System.Windows.Forms.TextBox()
-        Me.HisYazısı = New System.Windows.Forms.Label()
+        Me.DiaryBox = New System.Windows.Forms.TextBox()
         Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
         Me.HisButon1 = New System.Windows.Forms.RadioButton()
         Me.HisButon2 = New System.Windows.Forms.RadioButton()
         Me.HisButon3 = New System.Windows.Forms.RadioButton()
         Me.HisButon4 = New System.Windows.Forms.RadioButton()
         Me.HisButon5 = New System.Windows.Forms.RadioButton()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.AçToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Takvim = New System.Windows.Forms.DateTimePicker()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.MenuStrip1.SuspendLayout()
+        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'MenuStrip1
@@ -45,15 +49,15 @@ Partial Class Form1
         Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.DosyaToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(622, 30)
+        Me.MenuStrip1.Size = New System.Drawing.Size(582, 28)
         Me.MenuStrip1.TabIndex = 0
         Me.MenuStrip1.Text = "MenuStrip1"
         '
         'DosyaToolStripMenuItem
         '
-        Me.DosyaToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.KaydetToolStripMenuItem})
+        Me.DosyaToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AçToolStripMenuItem, Me.KaydetToolStripMenuItem})
         Me.DosyaToolStripMenuItem.Name = "DosyaToolStripMenuItem"
-        Me.DosyaToolStripMenuItem.Size = New System.Drawing.Size(64, 26)
+        Me.DosyaToolStripMenuItem.Size = New System.Drawing.Size(64, 24)
         Me.DosyaToolStripMenuItem.Text = "Dosya"
         '
         'KaydetToolStripMenuItem
@@ -62,35 +66,31 @@ Partial Class Form1
         Me.KaydetToolStripMenuItem.Size = New System.Drawing.Size(224, 26)
         Me.KaydetToolStripMenuItem.Text = "Kaydet"
         '
-        'TextBox
+        'DiaryBox
         '
-        Me.TextBox.BackColor = System.Drawing.Color.FromArgb(CType(CType(196, Byte), Integer), CType(CType(163, Byte), Integer), CType(CType(192, Byte), Integer))
-        Me.TextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.TextBox.Font = New System.Drawing.Font("Cascadia Mono SemiBold", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
-        Me.TextBox.Location = New System.Drawing.Point(298, 69)
-        Me.TextBox.Multiline = True
-        Me.TextBox.Name = "TextBox"
-        Me.TextBox.Size = New System.Drawing.Size(312, 369)
-        Me.TextBox.TabIndex = 1
-        Me.TextBox.Text = "Gününü burada not alabilirsin."
-        Me.ToolTip1.SetToolTip(Me.TextBox, "Günlük Not Yeri")
-        '
-        'HisYazısı
-        '
-        Me.HisYazısı.AutoSize = True
-        Me.HisYazısı.Font = New System.Drawing.Font("Cascadia Mono SemiBold", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
-        Me.HisYazısı.Location = New System.Drawing.Point(12, 42)
-        Me.HisYazısı.Name = "HisYazısı"
-        Me.HisYazısı.Size = New System.Drawing.Size(43, 24)
-        Me.HisYazısı.TabIndex = 2
-        Me.HisYazısı.Text = "His"
+        Me.DiaryBox.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.DiaryBox.BackColor = System.Drawing.Color.FromArgb(CType(CType(196, Byte), Integer), CType(CType(163, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.DiaryBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.DiaryBox.Font = New System.Drawing.Font("Cascadia Mono SemiBold", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.DiaryBox.Location = New System.Drawing.Point(276, 43)
+        Me.DiaryBox.Multiline = True
+        Me.DiaryBox.Name = "DiaryBox"
+        Me.DiaryBox.Size = New System.Drawing.Size(294, 295)
+        Me.DiaryBox.TabIndex = 1
+        Me.DiaryBox.Text = "Gününü burada not alabilirsin."
+        Me.ToolTip1.SetToolTip(Me.DiaryBox, "Günlük Not Yeri")
         '
         'HisButon1
         '
+        Me.HisButon1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.HisButon1.AutoSize = True
         Me.HisButon1.FlatStyle = System.Windows.Forms.FlatStyle.System
         Me.HisButon1.Font = New System.Drawing.Font("Cascadia Mono SemiBold", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
-        Me.HisButon1.Location = New System.Drawing.Point(12, 69)
+        Me.HisButon1.Location = New System.Drawing.Point(3, 6)
         Me.HisButon1.Name = "HisButon1"
         Me.HisButon1.Size = New System.Drawing.Size(117, 29)
         Me.HisButon1.TabIndex = 3
@@ -100,10 +100,13 @@ Partial Class Form1
         '
         'HisButon2
         '
+        Me.HisButon2.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.HisButon2.AutoSize = True
         Me.HisButon2.FlatStyle = System.Windows.Forms.FlatStyle.System
         Me.HisButon2.Font = New System.Drawing.Font("Cascadia Mono SemiBold", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
-        Me.HisButon2.Location = New System.Drawing.Point(12, 104)
+        Me.HisButon2.Location = New System.Drawing.Point(3, 41)
         Me.HisButon2.Name = "HisButon2"
         Me.HisButon2.Size = New System.Drawing.Size(73, 29)
         Me.HisButon2.TabIndex = 4
@@ -113,10 +116,13 @@ Partial Class Form1
         '
         'HisButon3
         '
+        Me.HisButon3.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.HisButon3.AutoSize = True
         Me.HisButon3.FlatStyle = System.Windows.Forms.FlatStyle.System
         Me.HisButon3.Font = New System.Drawing.Font("Cascadia Mono SemiBold", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
-        Me.HisButon3.Location = New System.Drawing.Point(12, 139)
+        Me.HisButon3.Location = New System.Drawing.Point(3, 76)
         Me.HisButon3.Name = "HisButon3"
         Me.HisButon3.Size = New System.Drawing.Size(150, 29)
         Me.HisButon3.TabIndex = 5
@@ -126,10 +132,13 @@ Partial Class Form1
         '
         'HisButon4
         '
+        Me.HisButon4.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.HisButon4.AutoSize = True
         Me.HisButon4.FlatStyle = System.Windows.Forms.FlatStyle.System
         Me.HisButon4.Font = New System.Drawing.Font("Cascadia Mono SemiBold", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
-        Me.HisButon4.Location = New System.Drawing.Point(12, 174)
+        Me.HisButon4.Location = New System.Drawing.Point(3, 111)
         Me.HisButon4.Name = "HisButon4"
         Me.HisButon4.Size = New System.Drawing.Size(84, 29)
         Me.HisButon4.TabIndex = 6
@@ -139,10 +148,13 @@ Partial Class Form1
         '
         'HisButon5
         '
+        Me.HisButon5.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.HisButon5.AutoSize = True
         Me.HisButon5.FlatStyle = System.Windows.Forms.FlatStyle.System
         Me.HisButon5.Font = New System.Drawing.Font("Cascadia Mono SemiBold", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
-        Me.HisButon5.Location = New System.Drawing.Point(12, 209)
+        Me.HisButon5.Location = New System.Drawing.Point(3, 146)
         Me.HisButon5.Name = "HisButon5"
         Me.HisButon5.Size = New System.Drawing.Size(128, 29)
         Me.HisButon5.TabIndex = 7
@@ -150,29 +162,62 @@ Partial Class Form1
         Me.HisButon5.Text = "Çok Kötü"
         Me.HisButon5.UseVisualStyleBackColor = True
         '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Takvim)
+        Me.Panel1.Controls.Add(Me.HisButon5)
+        Me.Panel1.Controls.Add(Me.HisButon1)
+        Me.Panel1.Controls.Add(Me.HisButon2)
+        Me.Panel1.Controls.Add(Me.HisButon3)
+        Me.Panel1.Controls.Add(Me.HisButon4)
+        Me.Panel1.Location = New System.Drawing.Point(8, 43)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(262, 201)
+        Me.Panel1.TabIndex = 8
+        '
+        'AçToolStripMenuItem
+        '
+        Me.AçToolStripMenuItem.Name = "AçToolStripMenuItem"
+        Me.AçToolStripMenuItem.Size = New System.Drawing.Size(224, 26)
+        Me.AçToolStripMenuItem.Text = "Aç"
+        '
+        'Takvim
+        '
+        Me.Takvim.Location = New System.Drawing.Point(3, 176)
+        Me.Takvim.Name = "Takvim"
+        Me.Takvim.Size = New System.Drawing.Size(200, 22)
+        Me.Takvim.TabIndex = 9
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(69, 258)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(142, 76)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "Fontu Değiştir"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(176, Byte), Integer), CType(CType(132, Byte), Integer), CType(CType(171, Byte), Integer))
-        Me.ClientSize = New System.Drawing.Size(622, 450)
-        Me.Controls.Add(Me.HisButon5)
-        Me.Controls.Add(Me.HisButon4)
-        Me.Controls.Add(Me.HisButon3)
-        Me.Controls.Add(Me.HisButon2)
-        Me.Controls.Add(Me.HisButon1)
-        Me.Controls.Add(Me.HisYazısı)
-        Me.Controls.Add(Me.TextBox)
+        Me.ClientSize = New System.Drawing.Size(582, 353)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.DiaryBox)
         Me.Controls.Add(Me.MenuStrip1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.MaximizeBox = False
-        Me.MaximumSize = New System.Drawing.Size(640, 497)
-        Me.MinimumSize = New System.Drawing.Size(640, 497)
+        Me.MaximumSize = New System.Drawing.Size(800, 600)
+        Me.MinimumSize = New System.Drawing.Size(600, 400)
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "DiaryApp"
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -181,12 +226,15 @@ Partial Class Form1
     Friend WithEvents MenuStrip1 As MenuStrip
     Friend WithEvents DosyaToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents KaydetToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents TextBox As TextBox
-    Friend WithEvents HisYazısı As Label
+    Friend WithEvents DiaryBox As TextBox
     Friend WithEvents ToolTip1 As ToolTip
     Friend WithEvents HisButon1 As RadioButton
     Friend WithEvents HisButon2 As RadioButton
     Friend WithEvents HisButon3 As RadioButton
     Friend WithEvents HisButon4 As RadioButton
     Friend WithEvents HisButon5 As RadioButton
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents AçToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Takvim As DateTimePicker
+    Friend WithEvents Button1 As Button
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,52 +1,93 @@
-﻿Imports System.Runtime.InteropServices
+﻿Imports System.IO
+Imports System.Runtime.InteropServices
 Imports System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel
+Imports Newtonsoft.Json
 
 Public Class Form1
-
-    Dim userName As String
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        HisYazısı.Text = "Kendini Nasıl Hissediyorsun?"
-        userName = InputBox("Adın nedir?", "İsim Öğrenme", "O")
-    End Sub
-    Private Sub HisButon1_CheckedChanged(sender As Object, e As EventArgs) Handles HisButon1.CheckedChanged
-        HisYazısı.Text = userName & " kendini çok iyi hissediyor"
-    End Sub
-    Private Sub HisButon2_CheckedChanged(sender As Object, e As EventArgs) Handles HisButon2.CheckedChanged
-        HisYazısı.Text = userName & " kendini iyi hissediyor"
-    End Sub
-    Private Sub HisButon3_CheckedChanged(sender As Object, e As EventArgs) Handles HisButon3.CheckedChanged
-        HisYazısı.Text = userName & " kendini fena hissetmiyor"
-    End Sub
-    Private Sub HisButon4_CheckedChanged(sender As Object, e As EventArgs) Handles HisButon4.CheckedChanged
-        HisYazısı.Text = userName & " kendini kötü hissediyor :("
-    End Sub
-    Private Sub HisButon5_CheckedChanged(sender As Object, e As EventArgs) Handles HisButon5.CheckedChanged
-        HisYazısı.Text = userName & " kendini çok kötü hissediyor :("
-    End Sub
+    Private Class HisDurumu
+        Public Property His As String
+        Public Property Metin As String
+        Public Property Tarih As DateTime
+    End Class
 
     Private Sub KaydetToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles KaydetToolStripMenuItem.Click
         Dim saveFileDialog1 As New SaveFileDialog With {
             .FileName = "GünlükDosyası",
-            .Filter = "Metin Dosyaları|*.txt|Tüm Dosyalar|*.*"
+            .Filter = "JSON dosyaları|*.json"
         }
         If saveFileDialog1.ShowDialog() = DialogResult.OK Then
             Dim dosyaYolu As String = saveFileDialog1.FileName
+            Dim secilenHis As String = ""
 
-            Dim dosya As System.IO.StreamWriter
-            dosya = My.Computer.FileSystem.OpenTextFileWriter(dosyaYolu, True, System.Text.Encoding.UTF8)
+            If HisButon1.Checked Then
+                secilenHis = "Çok İyi"
+            ElseIf HisButon2.Checked Then
+                secilenHis = "İyi"
+            ElseIf HisButon3.Checked Then
+                secilenHis = "Fena Değil"
+            ElseIf HisButon4.Checked Then
+                secilenHis = "Kötü"
+            ElseIf HisButon5.Checked Then
+                secilenHis = "Çok kötü"
+            End If
 
-            dosya.WriteLine(HisYazısı.Text & " bugün." & Environment.NewLine & "BUGUN NE OLDU: " & TextBox.Text)
+            Dim hisBilgi As New HisDurumu With {
+                .Tarih = Takvim.Value,
+                .Metin = DiaryBox.Text,
+                .His = secilenHis
+            }
 
-            dosya.Close()
+            File.WriteAllText(dosyaYolu, JsonConvert.SerializeObject(hisBilgi))
 
-            MessageBox.Show("Metin dosyaya başarıyla kaydedildi!",
-                            "Bilgi", MessageBoxButtons.OK,
-                            MessageBoxIcon.Information)
-        Else
-            MessageBox.Show("Kayıt Başarısız", "Dikkat",
-                            MessageBoxButtons.OK, MessageBoxIcon.Error)
-
+            MessageBox.Show("Günlük başarıyla kaydedildi.")
         End If
     End Sub
 
+    Private Sub AçToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AçToolStripMenuItem.Click
+        Dim openFileDialog1 As New OpenFileDialog With {
+            .FileName = "GünlükDosyası",
+            .Filter = "JSON dosyaları|*.json"
+        }
+        If openFileDialog1.ShowDialog() = DialogResult.OK Then
+            Dim dosyaYolu As String = openFileDialog1.FileName
+
+            If File.Exists(dosyaYolu) Then
+                Dim jsonVeri As String = File.ReadAllText(dosyaYolu)
+
+                Dim okunanHisBilgisi As HisDurumu = JsonConvert.DeserializeObject(Of HisDurumu)(jsonVeri)
+
+                DiaryBox.Text = okunanHisBilgisi.Metin
+
+                Takvim.Value = okunanHisBilgisi.Tarih
+
+                Select Case okunanHisBilgisi.His
+                    Case "Çok İyi"
+                        HisButon1.Checked = True
+                    Case "İyi"
+                        HisButon2.Checked = True
+                    Case "Fena Değil"
+                        HisButon3.Checked = True
+                    Case "Kötü"
+                        HisButon4.Checked = True
+                    Case "Çok kötü"
+                        HisButon5.Checked = True
+                End Select
+
+                MessageBox.Show($"Günlük içeriği ve his durumu başarıyla açıldı.", "Günlük Aç", MessageBoxButtons.OK)
+            Else
+                MessageBox.Show("Dosya bulunamadı.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            End If
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Using fontDialog As New FontDialog()
+            ' Kullanıcıya font seçme penceresini göster
+            If fontDialog.ShowDialog() = DialogResult.OK Then
+                ' Seçilen fontu metin kutusuna uygula
+                DiaryBox.Font = fontDialog.Font
+            End If
+        End Using
+    End Sub
 End Class
+
